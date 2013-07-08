64b23946eb1ce6a1736a49fa2ad86295dc257c4b
jmdelrio <jmdelrio@gmail.com>
2013-07-08 15:08:49 +0000

    Updated web.js with a buffer

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer (fs.readFileSync('./index.html'));
-  response.send(buffer.Tostring();
+  response.send(buffer.toString());
 });
 
 
