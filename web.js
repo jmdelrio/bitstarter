8963f02bc75ab5cb264d6ad12fc62fdba061a040
jmdelrio <jmdelrio@gmail.com>
2013-07-08 15:06:09 +0000

    Updated web.js with a buffer

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer (fs.readFileSync('./index.html'));
+  response.send(buffer.Tostring();
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
